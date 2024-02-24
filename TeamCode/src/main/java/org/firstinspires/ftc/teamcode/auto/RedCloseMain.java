d5c6bb1b37cf14f709658d327f3fe508550bd212
Infant Elfrick Gnanasusairaj <infant.elfrick@gmail.com>
2024-02-24 03:36:52 -0500

    auton update
    hehe

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auto/RedCloseMain.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auto/RedCloseMain.java
@@ -165,17 +165,17 @@ public class RedCloseMain extends LinearOpMode {
         if (color_zone == Zone.RIGHT) {
             start = bot.actionBuilder(new Pose2d(12, -64, Math.toRadians(270)))
                     .setTangent(Math.toRadians(0))
-                    .lineToX(35)
+                    .lineToX(32)
                     .setTangent(Math.toRadians(90))
-                    .splineToSplineHeading(new Pose2d(35,-36,Math.toRadians(0)),Math.toRadians(90))
+                    .splineToSplineHeading(new Pose2d(32,-36,Math.toRadians(0)),Math.toRadians(90))
 //                    .waitSeconds(1) //drop purple
                     .build();
-            plusZero = bot.actionBuilder(new Pose2d(35, -36, Math.toRadians(0)))
+            plusZero = bot.actionBuilder(new Pose2d(32, -36, Math.toRadians(0)))
                     .setTangent(Math.toRadians(-45))
-                    .splineToSplineHeading(new Pose2d(43.5,-42,Math.toRadians(180)),Math.toRadians(315))
+                    .splineToSplineHeading(new Pose2d(43.5,-48,Math.toRadians(180)),Math.toRadians(315))
 //                    .waitSeconds(1) //drop yellow
                     .build();
-            park = bot.actionBuilder(new Pose2d(43.5,-42,Math.toRadians(180)))
+            park = bot.actionBuilder(new Pose2d(43.5,-48,Math.toRadians(180)))
                     .setTangent(Math.toRadians(270))
                     .lineToY(-64)
                     .setTangent(0)
